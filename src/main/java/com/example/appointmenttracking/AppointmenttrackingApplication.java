6ef3c444ac28e345087f8395f79b8a09c80df4bc
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-27 12:22:52 +0600

    initial commit

---
+++ src/main/java/com/example/appointmenttracking/AppointmenttrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointmenttracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmenttrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmenttrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appointmenttracking/AppointmenttrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appointmenttracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmenttrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
