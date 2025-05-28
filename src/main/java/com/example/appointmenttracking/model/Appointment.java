9feeb0574f972a2981d3b6fe50b518bb21d5301b
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-28 12:06:21 +0600

    model class

---
+++ src/main/java/com/example/appointmenttracking/model/Appointment.java
@@ -0,0 +1,38 @@
+package com.example.appointmenttracking.model;
+
+import com.example.appointmenttracking.model.AppointmentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate appointmentDate;
+
+    private Integer serialNumber;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private boolean delayReported;
+
+    @Enumerated(EnumType.STRING)
+    private AppointmentStatus status;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @ManyToOne
+    private User patient;
+}
